a001beefa82f02831db0f520959783b64274f147
Georg Hertkorn <georg.hertkorn@adwmainz.de>
2026-03-06 09:45:21 +0100

    Update project admin data

--- src/Jlpigraf.jl
+++ src/Jlpigraf.jl
@@ -1,8 +1,11 @@
 module Jlpigraf
 
-using DataFrames, HTTP
 
-export api_setup, api_fetch
+using HTTP, URIs
+using JSON3, CSV
+using DataFrames
+
+export api_setup, api_fetch, fetch_table
 
 include("api.jl")
 include("fetch.jl")
